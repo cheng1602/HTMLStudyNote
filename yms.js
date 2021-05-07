384b69f85960e102fe2d776b8fb786488d9e0aeb
ChengX1 <77869646+cheng1602@users.noreply.github.com>
2021-05-07 18:05:55 +0800

    Add files via upload

---
+++ yms.js
@@ -0,0 +1,562 @@
+//获取接口调用url
+var hns_url = ""
+$.ajax({
+	url: baseUrl + "/dj/v1/config",
+	dataType: "json",
+	type: "GET",
+	async: false,
+	success: function(result) {
+		if (result && result.success) {
+			flag = true
+			hns_url = result.hns_url
+			dt_url = result.dt_url
+		}
+	}
+});
+//基层政务服务进驻部门与事项
+$.ajax({
+	url: hns_url + "/ymsdj/v1/bmsx/" + areaCode,
+	dataType: "json",
+	type: "GET",
+	async: true,
+	success: function(result) {
+		if (result.success) {
+			var Data = result.data;
+			var jzbm = [{
+				"xz_jd": Data[1].departNum,
+				"xz_jd_sxsl": Data[1].approveNum,
+				"c_sq": Data[2].departNum,
+				"c_sq_sxsl": Data[2].approveNum
+			}];
+			if (jzbm.length > 0) {
+				$("#xz_jd").text(jzbm[0].xz_jd);
+				$("#xz_jd_sxsl").text(jzbm[0].xz_jd_sxsl);
+				$("#c_sq").text(jzbm[0].c_sq);
+				$("#c_sq_sxsl").text(jzbm[0].c_sq_sxsl);
+			} else {
+				$("#xz_jd").text(0);
+				$("#xz_jd_sxsl").text(0);
+				$("#c_sq").text(0);
+				$("#c_sq_sxsl").text(0);
+			}
+		}
+	}
+});
+//各乡镇街道基层服务情况
+$.ajax({
+	url: hns_url + "/ymsdj/v1/jcfwqk/" + areaCode,
+	dataType: "json",
+	type: "GET",
+	async: true,
+	success: function(result) {
+		if (result.success) {
+			var Data = result.data;
+			var fwqk = [];
+			for (var i = 0; i < Data.length; i++) {
+				fwqk.push({
+					"dqmc": Data[i].showname,
+					"c_sq": Data[i].csl,
+					"sxzs": Data[i].sxzs,
+					"bjzs": Data[i].bjzs
+				})
+			}
+			var fwqk_gd = '';
+			clearInterval(fwqk_gd);
+			$('#fwqk_table_tbody tbody').empty();
+
+			if (fwqk.length != 0) {
+				for (var i = 0; i < fwqk.length; i++) {
+					$("#fwqk_table_tbody").append('<tr><td width="45%">' + toEllipsis(fwqk[i].dqmc, 14) +
+						'</td><td width="25%">' + fwqk[i].c_sq + '</td><td width="15%">' + fwqk[i].sxzs +
+						'</td><td width="15%">' + fwqk[i].bjzs + '</td></tr>')
+				}
+			}
+			if (fwqk.length > 7) {
+				$('#fwqk_table_tbody tbody').html($('#fwqk_table_tbody tbody').html() + $('#fwqk_table_tbody tbody').html());
+				$('#fwqk_table_tbody').css('top', '0');
+				var tblTop1 = 0;
+				var speed1 = 160; // 数值越大越慢
+				var outerHeight1 = $('#fwqk_table_tbody tbody').find("tr").outerHeight();
+				//匀速滚动方法
+				function ys1() {
+					if (tblTop1 <= -outerHeight1 * fwqk.length) {
+						tblTop1 = 0;
+					} else {
+						tblTop1 -= 2;
+					}
+					$('#fwqk_table_tbody').css('top', tblTop1 + 'px');
+				}
+				fwqk_gd = setInterval(ys1, speed1);
+			}
+
+			// 鼠标移上去取消滚动事件
+			$("#fwqk_table_tbody tbody").hover(function() {
+				clearInterval(fwqk_gd);
+			}, function() {
+				clearInterval(fwqk_gd);
+				fwqk_gd = setInterval(ys1, speed1);
+			})
+		}
+	}
+});
+//各乡镇街道基层政务服务事项排名TOP5
+$.ajax({
+	url: hns_url + "/ymsdj/v1/jcfwqkTop5/" + areaCode,
+	dataType: "json",
+	type: "GET",
+	async: true,
+	success: function(result) {
+		if (result.success) {
+			var Data = result.data;
+			var fwpm_xAxisData = [toEllipsis(toLineFeed(Data[0].showname), 5), toEllipsis(toLineFeed(Data[1].showname), 5),
+				toEllipsis(toLineFeed(Data[2].showname), 5), toEllipsis(toLineFeed(Data[3].showname), 5), toEllipsis(toLineFeed(
+					Data[4].showname), 5)
+			];
+			var fwpm_sxzs_data = [Data[0].sxzs, Data[1].sxzs, Data[2].sxzs, Data[3].sxzs, Data[4].sxzs];
+			var fwpm_tbssqd_data = [Data[0].tbssqd, Data[1].tbssqd, Data[2].tbssqd, Data[3].tbssqd, Data[4].tbssqd];
+			var fwpm_fwqysx_data = [Data[0].fwqysx, Data[1].fwqysx, Data[2].fwqysx, Data[3].fwqysx, Data[4].fwqysx];
+			var myChart = echarts.init(document.getElementById('gxzjdjczwfwsxpmEchart'));
+
+			// 指定图表的配置项和数据
+			var option = {
+				tooltip: {
+					trigger: 'axis',
+					axisPointer: {
+						type: 'shadow'
+					}
+				},
+				legend: {
+					icon: 'roundRect',
+					top: '2%',
+					right: '5%',
+					itemWidth: 20,
+					itemHeight: 10,
+					itemGap: 25,
+					textStyle: {
+						color: '#fff'
+					}
+				},
+				grid: {
+					top: '20%',
+					right: '8%',
+					left: '8%',
+					bottom: '10%'
+				},
+				xAxis: [{
+					type: 'category',
+					data: fwpm_xAxisData,
+					axisLine: {
+						lineStyle: {
+							color: 'rgba(255,255,255,0.12)'
+						}
+					},
+					axisLabel: {
+						margin: 10,
+						color: '#fff',
+						textStyle: {
+							fontSize: 14
+						},
+					}
+				}],
+				yAxis: [{
+					type: 'value',
+					splitNumber: 4,
+					splitLine: {
+						lineStyle: {
+							color: 'rgba(255,255,255,0.12)'
+						}
+					},
+					axisLine: {
+						show: false,
+						lineStyle: {
+							color: "#fff"
+						},
+					},
+					nameTextStyle: {
+						color: "#fff"
+					},
+					splitArea: {
+						show: false
+					}
+				}],
+				series: [{
+					name: '事项总数',
+					type: 'line',
+					data: fwpm_sxzs_data,
+					lineStyle: {
+						normal: {
+							width: 5,
+							color: {
+								type: 'linear',
+
+								colorStops: [{
+									offset: 1,
+									color: '#d3a54d' // 100% 处的颜色
+								}],
+								globalCoord: false // 缺省为 false
+							},
+
+						}
+					},
+					itemStyle: {
+						normal: {
+							color: '#d3a54d'
+						}
+					},
+					smooth: true
+				}, {
+					name: '填报实施清单',
+					type: 'line',
+					data: fwpm_tbssqd_data,
+					lineStyle: {
+						normal: {
+							width: 5,
+							color: {
+								type: 'linear',
+
+								colorStops: [{
+									offset: 1,
+									color: '#0debfb' // 100% 处的颜色
+								}],
+								globalCoord: false // 缺省为 false
+							},
+						}
+					},
+					itemStyle: {
+						normal: {
+							color: '#0debfb'
+						}
+					},
+					smooth: true
+				}, {
+					name: '服务前移事项',
+					type: 'line',
+					data: fwpm_fwqysx_data,
+					lineStyle: {
+						normal: {
+							width: 5,
+							color: {
+								type: 'linear',
+
+								colorStops: [{
+									offset: 1,
+									color: '#413eac' // 100% 处的颜色
+								}],
+								globalCoord: false // 缺省为 false
+							},
+						}
+					},
+					itemStyle: {
+						normal: {
+							color: '#413eac',
+						}
+					},
+					smooth: true
+				}]
+
+			};
+			myChart.setOption(option);
+		}
+	}
+});
+//基层办件总量
+$.ajax({
+	url: hns_url + "/ymsdj/v1/jcbjzl/" + areaCode,
+	dataType: "json",
+	type: "GET",
+	async: true,
+	success: function(result) {
+		if (result.success) {
+			var Data = result.data;
+			var midData = [{
+				"jczwfwbjzl": Data[0].bjzlTotal,
+				"jnjcbjl": Data[0].bjzlYear,
+				"byjcbjl": Data[0].bjzlMonth
+			}];
+			if (midData.length > 0) {
+				$("#jczwfwbjzl").text(toThousands(midData[0].jczwfwbjzl));
+				$("#jnjcbjl").text(toThousands(midData[0].jnjcbjl));
+				$("#byjcbjl").text(toThousands(midData[0].byjcbjl));
+			} else {
+				$("#jczwfwbjzl").text(0);
+				$("#jnjcbjl").text(0);
+				$("#byjcbjl").text(0);
+			}
+		}
+
+	}
+});
+
+//办件排名
+$.ajax({
+	url: hns_url + "/ymsdj/v1/cjbjpm/" + areaCode,
+	dataType: "json",
+	type: "GET",
+	async: true,
+	success: function(result) {
+		if (result.success) {
+			var Data = result.data;
+			var bjpm = [{
+					"bjpm_name": toEllipsis(getStreetName(Data[0].name), 4),
+					"bjpm_em": toPercent(Data[0].bjnum / Data[0].slnum),
+					"bjpm_num": Data[0].slnum
+				},
+				{
+					"bjpm_name": toEllipsis(getStreetName(Data[1].name), 4),
+					"bjpm_em": toPercent(Data[1].bjnum / Data[1].slnum),
+					"bjpm_num": Data[1].slnum
+				},
+				{
+					"bjpm_name": toEllipsis(getStreetName(Data[2].name), 4),
+					"bjpm_em": toPercent(Data[2].bjnum / Data[2].slnum),
+					"bjpm_num": Data[2].slnum
+				},
+				{
+					"bjpm_name": toEllipsis(getStreetName(Data[3].name), 4),
+					"bjpm_em": toPercent(Data[3].bjnum / Data[3].slnum),
+					"bjpm_num": Data[3].slnum
+				},
+				{
+					"bjpm_name": toEllipsis(getStreetName(Data[4].name), 4),
+					"bjpm_em": toPercent(Data[4].bjnum / Data[4].slnum),
+					"bjpm_num": Data[4].slnum
+				},
+			]
+			if (bjpm.length > 0) {
+				for (var i = 0; i < bjpm.length; i++) {
+					$("#bjpm" + i + "_name").text(bjpm[i].bjpm_name);
+					$("#bjpm" + i + "_em").css("width", bjpm[i].bjpm_em);
+					$("#bjpm" + i + "_num").text(bjpm[i].bjpm_num);
+				}
+			} else {
+				for (var i = 0; i < 5; i++) {
+					$("#bjpm" + i + "_name").text('');
+					$("#bjpm" + i + "_em").css("width", "0%");
+					$("#bjpm" + i + "_num").text(0);
+				}
+			}
+
+		}
+	}
+});
+//基层政务服务办件排名Top5
+$.ajax({
+	url: hns_url + "/ymsdj/v1/jcbjTop5/" + areaCode,
+	dataType: "json",
+	type: "GET",
+	async: true,
+	success: function(result) {
+		if (result.success) {
+			var Data = result.data;
+			var bjpm_xAxisData = [toEllipsis(toLineFeed(Data[0].showname), 5), toEllipsis(toLineFeed(Data[1].showname), 5),
+				toEllipsis(toLineFeed(Data[2].showname), 5), toEllipsis(toLineFeed(Data[3].showname), 5), toEllipsis(toLineFeed(
+					Data[4].showname), 5)
+			];
+			var bjpm_data = [Data[0].bjzlTotal, Data[1].bjzlTotal, Data[2].bjzlTotal, Data[3].bjzlTotal, Data[4].bjzlTotal];
+			var myChart = echarts.init(document.getElementById('jczwfwbjpmEchart'));
+			// 指定图表的配置项和数据
+			var option = {
+
+
+				tooltip: {
+					trigger: 'axis',
+					axisPointer: {
+						type: 'shadow'
+					}
+				},
+				grid: {
+					top: '15%',
+					right: '7%',
+					left: '7%',
+					bottom: '12%'
+				},
+				xAxis: [{
+					type: 'category',
+					data: bjpm_xAxisData,
+					axisLine: {
+						lineStyle: {
+							color: 'rgba(255,255,255,0.12)'
+						}
+					},
+					axisLabel: {
+						margin: 10,
+						color: '#e2e9ff',
+						textStyle: {
+							fontSize: 14
+						},
+					},
+				}],
+				yAxis: [{
+					axisLine: {
+						show: false,
+						lineStyle: {
+							color: 'rgba(255,255,255,1)'
+						}
+					},
+					splitLine: {
+						lineStyle: {
+							color: 'rgba(255,255,255,0.12)'
+						}
+					}
+				}],
+				series: [{
+					type: 'bar',
+					data: bjpm_data,
+					barWidth: '20px',
+					itemStyle: {
+						normal: {
+							color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+								offset: 0,
+								color: '#04f7e3' // 0% 处的颜色
+							}, {
+								offset: 1,
+								color: 'rgba(11, 57, 120, 0.1)' // 100% 处的颜色
+							}], false),
+							barBorderRadius: [30, 30, 30, 30],
+						}
+					}
+				}]
+			};
+
+			// 使用刚指定的配置项和数据显示图表。
+			myChart.setOption(option);
+		}
+	}
+});
+//热门事项top5
+$.ajax({
+	url: hns_url + "/ymsdj/v1/rmsxTop5/" + areaCode,
+	dataType: "json",
+	type: "GET",
+	async: true,
+	success: function(result) {
+		if (result.success) {
+			var Data = result.data;
+			var rmpm = [{
+					"rmpm_name": toEllipsis(Data[0].approveName, 14),
+					"rmpm_sll": Data[0].acceptCount,
+					"rmpm_bjs": Data[0].handleCount
+				},
+				{
+					"rmpm_name": toEllipsis(Data[1].approveName, 14),
+					"rmpm_sll": Data[1].acceptCount,
+					"rmpm_bjs": Data[1].handleCount
+				},
+				{
+					"rmpm_name": toEllipsis(Data[2].approveName, 14),
+					"rmpm_sll": Data[2].acceptCount,
+					"rmpm_bjs": Data[2].handleCount
+				},
+				{
+					"rmpm_name": toEllipsis(Data[3].approveName, 14),
+					"rmpm_sll": Data[3].acceptCount,
+					"rmpm_bjs": Data[3].handleCount
+				},
+				{
+					"rmpm_name": toEllipsis(Data[4].approveName, 14),
+					"rmpm_sll": Data[4].acceptCount,
+					"rmpm_bjs": Data[4].handleCount
+				}
+			]
+			if (rmpm.length > 0) {
+				for (var i = 0; i < rmpm.length; i++) {
+					$("#rmpm" + i + "_name").text(rmpm[i].rmpm_name);
+					$("#rmpm" + i + "_sll").text(rmpm[i].rmpm_sll);
+					$("#rmpm" + i + "_bjs").text(rmpm[i].rmpm_bjs);
+				}
+			} else {}
+		}
+	}
+});
+//发牌情况top5
+$.ajax({
+	url: hns_url + "/ymsdj/v1/fpqkTop5/" + areaCode,
+	dataType: "json",
+	type: "GET",
+	async: true,
+	success: function(result) {
+		if (result.success) {
+			var Data = result.data;
+			var fptj = [{
+				"fptj_name": toEllipsis(Data[0].showname, 14),
+				"fptj_hongpai": Data[0].redSum,
+				"fptj_huangpai": Data[0].greenSum,
+				"fptj_yujing": Data[0].ranking
+			}, {
+				"fptj_name": toEllipsis(Data[1].showname, 14),
+				"fptj_hongpai": Data[1].redSum,
+				"fptj_huangpai": Data[1].greenSum,
+				"fptj_yujing": Data[1].ranking
+			}, {
+				"fptj_name": toEllipsis(Data[2].showname, 14),
+				"fptj_hongpai": Data[2].redSum,
+				"fptj_huangpai": Data[2].greenSum,
+				"fptj_yujing": Data[2].ranking
+			}, {
+				"fptj_name": toEllipsis(Data[3].showname, 14),
+				"fptj_hongpai": Data[3].redSum,
+				"fptj_huangpai": Data[3].greenSum,
+				"fptj_yujing": Data[3].ranking
+			}, {
+				"fptj_name": toEllipsis(Data[4].showname, 14),
+				"fptj_hongpai": Data[4].redSum,
+				"fptj_huangpai": Data[4].greenSum,
+				"fptj_yujing": Data[4].ranking
+			}]
+			if (fptj.length > 0) {
+				for (var i = 0; i < fptj.length; i++) {
+					$("#fptj" + i + "_name").text(fptj[i].fptj_name);
+					$("#fptj" + i + "_hongpai").text(fptj[i].fptj_hongpai);
+					$("#fptj" + i + "_huangpai").text(fptj[i].fptj_huangpai);
+					$("#fptj" + i + "_yujing").text(fptj[i].fptj_yujing);
+				}
+			} else {}
+		}
+	}
+});
+
+//街道名换行
+function toLineFeed(str) {
+	if (str.indexOf("县") != -1) {
+		var index = str.lastIndexOf("县");
+		str = str.substring(index + 1, str.length);
+	}
+	// if (str.indexOf("县") != -1) {
+	// 	var index = str.lastIndexOf("县");
+	// 	str1 = str.substring(0, index + 1);
+	// 	str2 = str.substring(index + 1, str.length);
+	// 	str = str1 + '\n' + str2;
+	// }
+	return str;
+}
+//截取社区名
+function getStreetName(str) {
+	if (str.indexOf("镇") != -1) {
+		var index = str.lastIndexOf("镇");
+		str = str.substring(index + 1, str.length);
+	} else if (str.indexOf("街道") != -1) {
+		var index = str.lastIndexOf("街道");
+		str = str.substring(index + 2, str.length);
+	} else if (str.indexOf("乡") != -1) {
+		var index = str.lastIndexOf("乡");
+		str = str.substring(index + 1, str.length);
+	}
+	return str;
+}
+//超过长度省略
+function toEllipsis(str, leng) {
+	if (str.length > leng) {
+		str = str.substring(0, leng - 1) + "...";
+	}
+	return str;
+}
+//数字格式化
+function toThousands(num) {
+	return (num || 0).toString().replace(/(\d)(?=(?:\d{3})+$)/g, '$1,');
+}
+//百分比转换
+function toPercent(point) {
+	var str = Number(point * 100).toFixed(1);
+	str += "%";
+	return str;
+}
---
+++ yms.jsp
@@ -0,0 +1,281 @@
+<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+		<link rel="stylesheet" href="css/base.css" />
+		<link rel="stylesheet" href="css/yimenshi.css" />
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript">
+			var baseUrl = "<%=request.getContextPath()%>";
+			var areaCode = "<%=request.getParameter("areaCode")%>"
+		</script>
+
+		<style type="text/css">
+			@font-face {
+				font-family: 'font1';
+				src: url(img/DIN.eot?v=1.0.2);
+				src: url(img/DIN.eot?v=1.0.2#iefix) format('embedded-opentype'),
+					url(img/DIN.ttf?v=1.0.2) format('truetype')
+			}
+
+			@font-face {
+				font-family: 'font2';
+				src:
+					url(img/DS-DigitalBold.TTF?v=1.0.2) format('truetype')
+			}
+		</style>
+
+		<script src="<%=request.getContextPath()%>/dsjpt/js/echarts.min.js" type="text/javascript"></script>
+		<script src="<%=request.getContextPath()%>/dsjpt/js/public_llz.js" type="text/javascript"></script>
+
+
+	</head>
+	<body>
+		<div class="bg">
+			<div class="header">
+				<div class="header_lf">
+					<img src="img/logo.png" />
+				</div>
+				<div class="header_rg">
+					<a>总体概况</a>
+					<a>好差评</a>
+					<a>市政大厅</a>
+					<a class="active">一门式</a>
+				</div>
+			</div>
+
+			<div class="main clearfix">
+				<div class="left">
+					<!--基层政务服务进驻部门与事项-->
+					<div class="jinZhuBuMen">
+						<div class="titleBox">基层政务服务进驻部门与事项</div>
+						<div class="jinZhuBuMenBox">
+							<div class="jinZhuBuMenItem">
+								<img src="img/yms_jinZhuBuMenItem1.png" alt="" />
+								<em>
+									<span id="xz_jd"></span>
+									<font>乡镇(街道)</font>
+								</em>
+								<em>
+									<span id="xz_jd_sxsl"></span>
+									<font>事项数量</font>
+								</em>
+							</div>
+							<div class="jinZhuBuMenItem">
+								<img src="img/yms_jinZhuBuMenItem2.png" alt="" />
+								<em>
+									<span id="c_sq"></span>
+									<font>村(社区)</font>
+								</em>
+								<em>
+									<span id="c_sq_sxsl"></span>
+									<font>事项数量</font>
+								</em>
+							</div>
+						</div>
+					</div>
+
+					<!--各乡镇街道基层服务情况-->
+					<div class="fuWuQingKuang">
+						<div class="titleBox">各乡镇街道基层服务情况</div>
+						<div class="fwqk_tb_header">
+							<table border="0" cellspacing="0" cellpadding="0" class="fwqk_table_thead" id="fwqk_table_thead">
+
+								<thead>
+									<tr>
+										<th width="45%">地区名称</th>
+										<th width="25%">村（社区）</th>
+										<th width="15%">事项总数</th>
+										<th width="15%">办件总数</th>
+									</tr>
+								</thead>
+							</table>
+
+						</div>
+						<div id="outer" style="overflow: hidden; height: 200px;">
+							<div class="fwqk_roll">
+								<table border="0" cellspacing="0" cellpadding="0" class="fwqk_table_tbody" id="fwqk_table_tbody">
+									<tbody id="fwqk_tbody">
+									</tbody>
+								</table>
+							</div>
+						</div>
+
+					</div>
+
+					<!--各乡镇街道基层政务服务事项排名TOP5-->
+					<div class="shiXiangPaiMing">
+						<div class="titleBox">各乡镇街道基层政务服务事项排名TOP5</div>
+						<div class="shiXiangPaiMing_charts" id="gxzjdjczwfwsxpmEchart">
+							<!-- <p class="boxP">删除p标签，此div中放置表格，颜色：#d3a54d,#0debfb,#413eac,</p> -->
+						</div>
+					</div>
+				</div>
+
+				<div class="mid">
+					<div class="midData">
+						<div class="midDataItem1">
+							<span id="jczwfwbjzl"></span>
+							<font>基层政务服务办件总量</font>
+						</div>
+						<div class="midDataItem2">
+							<span id="jnjcbjl"></span>
+							<font>今年基层办件量</font>
+						</div>
+						<div class="midDataItem2 midDataItem3">
+							<span id="byjcbjl"></span>
+							<font>本月基层办件量</font>
+						</div>
+					</div>
+
+					<!--办件排名-->
+					<div class="banJian">
+						<div class="titleBox">办件排名</div>
+						<div class="cunJiBanJianList">
+							<div class="cunJiBanJianItem">
+								<div class="index"><img src="img/paiming1.png" alt="" /></div>
+								<div class="site"><span id="bjpm0_name"></span></div>
+								<div class="step">
+									<em style="width: 59%;" id="bjpm0_em"><b></b></em>
+								</div>
+								<div class="text"><b id="bjpm0_num"></b>件</div>
+							</div>
+							<div class="cunJiBanJianItem">
+								<div class="index"><img src="img/paiming2.png" alt="" /></div>
+								<div class="site"><span id="bjpm1_name"></span></div>
+								<div class="step">
+									<em style="width: 74%;" id="bjpm1_em"><b></b></em>
+								</div>
+								<div class="text"><b id="bjpm1_num"></b>件</div>
+							</div>
+							<div class="cunJiBanJianItem">
+								<div class="index"><img src="img/paiming3.png" alt="" /></div>
+								<div class="site"><span id="bjpm2_name"></span></div>
+								<div class="step">
+									<em style="width: 74%;" id="bjpm2_em"><b></b></em>
+								</div>
+								<div class="text"><b id="bjpm2_num"></b>件</div>
+							</div>
+							<div class="cunJiBanJianItem">
+								<div class="index"><img src="img/paiming4.png" alt="" /></div>
+								<div class="site"><span id="bjpm3_name"></span></div>
+								<div class="step">
+									<em style="width: 74%;" id="bjpm3_em"><b></b></em>
+								</div>
+								<div class="text"><b id="bjpm3_num"></b>件</div>
+							</div>
+							<div class="cunJiBanJianItem">
+								<div class="index"><img src="img/paiming5.png" alt="" /></div>
+								<div class="site"><span id="bjpm4_name"></span></div>
+								<div class="step">
+									<em style="width: 74%;" id="bjpm4_em"><b></b></em>
+								</div>
+								<div class="text"><b id="bjpm4_num"></b>件</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="right">
+					<!--基层政务服务办件排名-->
+					<div class="banJianPaiMing">
+						<div class="titleBox">基层政务服务办件排名</div>
+						<div class="banJianPaiMing_charts" id="jczwfwbjpmEchart">
+							<!-- <p class="boxP">删除p标签，此div中放置表格，颜色：#04f7e3</p> -->
+						</div>
+					</div>
+
+					<!--热门事项TOP5-->
+					<div class="reMenShiXiang">
+						<div class="titleBox">热门事项TOP5</div>
+						<div class="reMenShiXiangBox">
+							<div class="reMenShiXiangItem">
+								<div class="reMenShiXiangIndex">1</div>
+								<span id="rmpm0_name" style="width:70%;margin:0 auto"></span>
+								<font>受理量<b id="rmpm0_sll"></b></font>
+								<font>办结数<b id="rmpm0_bjs"></b></font>
+							</div>
+							<div class="reMenShiXiangItem">
+								<div class="reMenShiXiangIndex">2</div>
+								<span id="rmpm1_name" style="width:70%;margin:0 auto"></span>
+								<font>受理量<b id="rmpm1_sll"></b></font>
+								<font>办结数<b id="rmpm1_bjs"></b></font>
+							</div>
+							<div class="reMenShiXiangItem">
+								<div class="reMenShiXiangIndex">3</div>
+								<span id="rmpm2_name" style="width:70%;margin:0 auto"></span>
+								<font>受理量<b id="rmpm2_sll"></b></font>
+								<font>办结数<b id="rmpm2_bjs"></b></font>
+							</div>
+							<div class="reMenShiXiangItem">
+								<div class="reMenShiXiangIndex">4</div>
+								<span id="rmpm3_name" style="width:70%;margin:0 auto"></span>
+								<font>受理量<b id="rmpm3_sll"></b></font>
+								<font>办结数<b id="rmpm3_bjs"></b></font>
+							</div>
+							<div class="reMenShiXiangItem">
+								<div class="reMenShiXiangIndex">5</div>
+								<span id="rmpm4_name" style="width:70%;margin:0 auto"></span>
+								<font>受理量<b id="rmpm4_sll"></b></font>
+								<font>办结数<b id="rmpm4_bjs"></b></font>
+							</div>
+						</div>
+					</div>
+
+					<!--发牌统计情况TOP5-->
+					<div class="faPaiTongJi">
+						<div class="titleBox">发牌统计情况TOP5</div>
+						<table border="0" cellspacing="0" cellpadding="0">
+							<tr>
+								<th>排名</th>
+								<th>区域</th>
+								<th>红牌数</th>
+								<th>黄牌数</th>
+								<th>预警数</th>
+							</tr>
+							<tr>
+								<td><img src="img/paiming1.png" /></td>
+								<td id="fptj0_name"></td>
+								<td id="fptj0_hongpai"></td>
+								<td id="fptj0_huangpai"></td>
+								<td id="fptj0_yujing"></td>
+							</tr>
+							<tr>
+								<td><img src="img/paiming2.png" /></td>
+								<td id="fptj1_name"></td>
+								<td id="fptj1_hongpai"></td>
+								<td id="fptj1_huangpai"></td>
+								<td id="fptj1_yujing"></td>
+							</tr>
+							<tr>
+								<td><img src="img/paiming3.png" /></td>
+								<td id="fptj2_name"></td>
+								<td id="fptj2_hongpai"></td>
+								<td id="fptj2_huangpai"></td>
+								<td id="fptj2_yujing"></td>
+							</tr>
+							<tr>
+								<td><img src="img/paiming4.png" /></td>
+								<td id="fptj3_name"></td>
+								<td id="fptj3_hongpai"></td>
+								<td id="fptj3_huangpai"></td>
+								<td id="fptj3_yujing"></td>
+							</tr>
+							<tr>
+								<td><img src="img/paiming5.png" /></td>
+								<td id="fptj4_name"></td>
+								<td id="fptj4_hongpai"></td>
+								<td id="fptj4_huangpai"></td>
+								<td id="fptj4_yujing"></td>
+							</tr>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+	<script type="text/javascript" src="js/yms.js"></script>
+
+</html>
